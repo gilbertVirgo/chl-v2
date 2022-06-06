d8733c61650be69d65c63d40c5251d61e1e36a69
Gil <gilbertjvirgo@gmail.com>
2022-06-06 19:06:30 +0100

    Gosh

--- frontend/src/pages/Walks/index.js
+++ frontend/src/pages/Walks/index.js
@@ -1,4 +1,4 @@
-import { Heading, Paragraph } from "../../components/Text";
+import { Heading, Paragraph, Subheading } from "../../components/Text";
 
 import Button from "../../components/Button";
 import Feature from "../../components/Feature";
@@ -19,36 +19,38 @@ export default () => (
 				</Paragraph>
 			</Hero>
 		</Section>
-		<Section explode>
-			<Feature>
-				<Feature.Image src={require("../../assets/walks-1.jpg")} />
-				<Feature.Title>London Day</Feature.Title>
-				<Feature.Body>
-					<Paragraph style={{ marginBottom: "30px" }}>
-						We take small groups, families, church groups and others
-						on walks and tours through some of the awesome London
-						gospel stories.
-					</Paragraph>
-					<Button>Apply for a London Day</Button>
-				</Feature.Body>
-			</Feature>
-			<Feature invert>
-				<Feature.Image src={require("../../assets/walks-2.jpg")} />
-				<Feature.Title>Other Tours</Feature.Title>
-				<Feature.Body>
-					<Paragraph style={{ marginBottom: "30px" }}>
-						Our other tours are the Gospel in Art in London&apos;s
-						major art galleries, and The Influence of Christianity
-						on the West in the V&amp;A.
-						<br />
-						<br />
-						If you would like to apply for one of these, or if you
-						would like only a City Walk or a British Museum tour, or
-						if you have any other questions please{" "}
-						<Link to="contact">email</Link>.
-					</Paragraph>
-				</Feature.Body>
-			</Feature>
+
+		<Section skinny>
+			<Heading>London Day</Heading>
+			<Subheading>
+				We give walking tours through London&apos;s church history and
+				the British Museum to groups as small as four people.
+			</Subheading>
+			<Paragraph>
+				There&apos;s amazing church history in the City of London with
+				sites associated with Wilberforce, Tyndale, Wesley, Whitefield,
+				Elizabeth Fry, John Newton and many others. Meanwhile in the
+				British Museum there are items associated with Abraham,
+				Hezekiah, Daniel, Esther, Nehemiah, Paul and others.
+				<br />
+				<br />A London Day includes a tour of the British Museum and a
+				guided walk in the City of London.
+			</Paragraph>
+			<Button>Apply for a London Day</Button>
+		</Section>
+		<Section skinny>
+			<Heading>Other Tours</Heading>
+			<Subheading>
+				Our other tours are the Gospel in Art in London&apos;s major art
+				galleries, and The Influence of Christianity on the West in the
+				V&amp;A.
+			</Subheading>
+			<Paragraph>
+				If you would like to apply for one of these, or if you would
+				like only a City Walk or a British Museum tour, or if you have
+				any other questions please{" "}
+				<a href="/contact/other-tour">get in touch</a>.
+			</Paragraph>
 		</Section>
 	</React.Fragment>
 );
